ef9828d340a7cfc8e680c8fafb28d9f361354060
Carmen Angélica García Pérez <cangelicagp@gmail.com>
2022-07-06 18:36:59 -0500

    Add project files.

---
+++ BL/Alumno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BL
+{
+    public class Alumno
+    {
+        public static ML.Result Add(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(SqlConnection context = new SqlConnection(DL.Conexion.Get()))
+                {
+                    string query = "INSERT INTO [Alumno] ([Nombre],[ApellidoPaterno],[ApellidoMaterno],[Email])VALUES (@Nombre, @ApellidoPaterno, @ApellidoMaterno, @Email)";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = context;
+                        cmd.CommandText = query;
+
+                        SqlParameter[] collection = new SqlParameter[4];
+
+                        collection[0] = new SqlParameter("Nombre", SqlDbType.VarChar);
+                        collection[0].Value = alumno.Nombre;
+
+                        collection[1] = new SqlParameter("ApellidoPaterno", SqlDbType.VarChar);
+                        collection[1].Value = alumno.ApellidoPaterno;
+
+                        collection[2] = new SqlParameter("ApellidoMaterno", SqlDbType.VarChar);
+                        collection[2].Value = alumno.ApellidoMaterno;
+
+                        collection[3] = new SqlParameter("Email", SqlDbType.VarChar);
+                        collection[3].Value = alumno.Email;
+
+                        cmd.Parameters.AddRange(collection);
+
+                        cmd.Connection.Open();
+
+                        int RowsAffected = cmd.ExecuteNonQuery();
+
+                        if(RowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct=false;
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Alumno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno();
+
+            Console.WriteLine("Ingrese el nombre");
+            alumno.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el ApellidoPaterno");
+            alumno.ApellidoPaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el ApellidoMaterno");
+            alumno.ApellidoMaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el Email");
+            alumno.Email = Console.ReadLine();
+
+            ML.Result result = BL.Alumno.Add(alumno);
+
+            if(result.Correct)
+            {
+                Console.WriteLine("Registro exitoso");
+              
+            }
+            else
+            {
+                Console.WriteLine("Ocurrio un error al realizar la insercion");
+            }
+        }
+    }
+}
