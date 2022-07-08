83526efa0810d50864442a91964e9eee6ef26c65
Carmen Angélica García Pérez <cangelicagp@gmail.com>
2022-07-08 13:18:04 -0500

    Add STORED PROCEDURE

--- BL/Alumno.cs
+++ BL/Alumno.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using ML;
 
 namespace BL
 {
@@ -12,7 +13,8 @@ namespace BL
     {
         public static ML.Result Add(ML.Alumno alumno)
         {
-            ML.Result result = new ML.Result();
+            ML.Result result = new ML.Result(); 
+
             try
             {
                 using(SqlConnection context = new SqlConnection(DL.Conexion.Get()))
@@ -63,5 +65,159 @@ namespace BL
             }
             return result;
         }
+
+        public static ML.Result Update(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(SqlConnection context = new SqlConnection(DL.Conexion.Get()))
+                {
+                    string query = "UPDATE [Alumno] SET [Nombre] = @Nombre,[ApellidoPaterno] = @ApellidoPaterno,[ApellidoMaterno] = @ApellidoMaterno,[Email] = @Email WHERE [IdAlumno] = @IdAlumno";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = context;
+                        cmd.CommandText = query;
+
+                        SqlParameter[] collection = new SqlParameter[5];
+
+                        collection[0] = new SqlParameter("IdAlumno", SqlDbType.Int);
+                        collection[0].Value = alumno.IdAlumno;
+
+                        collection[1] = new SqlParameter("Nombre", SqlDbType.VarChar);
+                        collection[1].Value = alumno.Nombre;
+
+                        collection[2] = new SqlParameter("ApellidoPaterno", SqlDbType.VarChar);
+                        collection[2].Value = alumno.ApellidoPaterno;
+
+                        collection[3] = new SqlParameter("ApellidoMaterno", SqlDbType.VarChar);
+                        collection[3].Value = alumno.ApellidoMaterno;
+
+                        collection[4] = new SqlParameter("Email", SqlDbType.VarChar);
+                        collection[4].Value = alumno.Email;
+
+                        cmd.Parameters.AddRange(collection);
+                        cmd.Connection.Open();
+
+                        int RowsAffected = cmd.ExecuteNonQuery();
+
+                        if (RowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct=false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result Delete(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(SqlConnection context = new SqlConnection(DL.Conexion.Get()))
+                {
+                    string query = "DELETE FROM [Alumno] WHERE IdAlumno= @IdAlumno";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+
+                        SqlParameter[] collection = new SqlParameter[1];
+
+                        collection[0] = new SqlParameter("IdAlumno", SqlDbType.Int);
+                        collection[0].Value = alumno.IdAlumno;
+
+                        cmd.Parameters.AddRange(collection);
+
+                        cmd.Connection.Open();
+                        int RowsAffected = cmd.ExecuteNonQuery();
+
+                        if(RowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct= false;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+
+
+        public static ML.Result AddSP(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result(); //INSTANCIA ES PARA CREAR OBJETOS QUE ME PERMITAN ACCEDER O MANIPULAR SUS METODOS Y ATRIBUTOS
+            try
+            {
+                using(SqlConnection context = new SqlConnection(DL.Conexion.Get()))
+                {
+                    string query = "AlumnoAdd";
+
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = context;
+                        cmd.CommandText = query;
+                        cmd.CommandType = CommandType.StoredProcedure;
+                    
+
+                        SqlParameter[] collection = new SqlParameter[4];
+
+                        collection[0] = new SqlParameter("Nombre", SqlDbType.VarChar);
+                        collection[0].Value = alumno.Nombre;
+
+                        collection[1] = new SqlParameter("ApellidoPaterno", SqlDbType.VarChar);
+                        collection[1].Value = alumno.ApellidoPaterno;
+
+                        collection[2] = new SqlParameter("ApellidoMaterno", SqlDbType.VarChar);
+                        collection[2].Value = alumno.ApellidoMaterno;
+
+                        collection[3] = new SqlParameter("Email", SqlDbType.VarChar);
+                        collection[3].Value = alumno.Email;
+
+                        cmd.Parameters.AddRange(collection);
+                        cmd.Connection.Open();
+
+                        int RowsAffected = cmd.ExecuteNonQuery();
+
+                        if(RowsAffected > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct=false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+
+        }
+          
     }
 }
--- PL/Alumno.cs
+++ PL/Alumno.cs
@@ -10,10 +10,11 @@ namespace PL
     {
         public static void Add()
         {
-            ML.Alumno alumno = new ML.Alumno();
+            ML.Alumno alumno = new ML.Alumno();  //INSTANCIA PARA CREAR OBJETOS QUE ME PERMITAN INTERACTUAR CON LOS MÉTODOS Y ATRIBUTOS DE LA CLASE
 
             Console.WriteLine("Ingrese el nombre");
             alumno.Nombre = Console.ReadLine();
+
 
             Console.WriteLine("Ingrese el ApellidoPaterno");
             alumno.ApellidoPaterno = Console.ReadLine();
@@ -24,17 +25,76 @@ namespace PL
             Console.WriteLine("Ingrese el Email");
             alumno.Email = Console.ReadLine();
 
-            ML.Result result = BL.Alumno.Add(alumno);
 
-            if(result.Correct)
+            //ML.Result result = BL.Alumno.Add(alumno);
+            ML.Result result = BL.Alumno.AddSP(alumno);
+
+            if (result.Correct)
             {
                 Console.WriteLine("Registro exitoso");
-              
+
             }
             else
             {
                 Console.WriteLine("Ocurrio un error al realizar la insercion");
             }
         }
+
+        public static void Update()
+        {
+            ML.Alumno alumno = new ML.Alumno();
+
+            Console.WriteLine("Ingresa el Id");
+            alumno.IdAlumno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el Nombre");
+            alumno.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa el ApellidoPaterno");
+            alumno.ApellidoPaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingresa el ApellidoMaterno");
+            alumno.ApellidoMaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingresa el email");
+            alumno.Email = Console.ReadLine();
+
+            ML.Result result = BL.Alumno.Update(alumno);
+
+            if (result.Correct)
+            {
+                Console.WriteLine("El registro se actualizó correctamente");
+            }
+            else
+            {
+                Console.WriteLine("Ocurrio un error");
+            }
+
+
+        }
+
+        public static void Delete()
+        {
+            ML.Alumno alumno = new ML.Alumno();
+
+            Console.WriteLine("Ingrese el Id del registro a eliminar");
+            alumno.IdAlumno = int.Parse(Console.ReadLine());
+
+            ML.Result result = BL.Alumno.Delete(alumno);
+
+            if (result.Correct)
+            {
+                Console.WriteLine("El registro se elimino correctamente");
+            }
+            else
+            {
+                Console.WriteLine("Ocurrio un error al eliminar el registro");
+            }
+        }
+
+
     }
+
+
+        
 }
